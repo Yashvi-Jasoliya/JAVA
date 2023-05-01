9610fc33da12dd8383d58a27b9b5ee7dc156f783
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-05-01 21:18:34 +0530

    Add files via upload

---
+++ ch_1_Vars_and_Datatypes/L_02_Sum.java
@@ -0,0 +1,19 @@
+package ch_1_Vars_and_Datatypes;
+
+public class L_02_Sum {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!\n");
+        System.out.println("The sum of three numbers is :");
+        int a, b, c;
+        a = 20;
+        b = 10;
+        c = 5;
+        int sum = a + b + c;
+        System.out.println("Sum :- " + sum);
+    }
+}
+
+//add two num
+//AddTwoNumber --> pascal naming convention use for class
+//addTwoNumber --> camel case naming convention use for function
