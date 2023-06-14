79bbd237fb8bb06b4554012bd83b119a77f28b51
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-14 21:34:58 +0530

    Add files via upload

---
+++ Practicals_Basic 1 to10/pr_1.java
@@ -0,0 +1,12 @@
+package clg_pr_1_to_10;
+
+public class pr_1 {
+    public static void main(String[] args) {
+
+        System.out.println("First Name : abc");
+        System.out.println("Second Name : pqr");
+        System.out.println("Last Name : str");
+        }
+
+    }
+
