b3d2a483680aada9d0f4d7fd4800973848801955
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-14 21:38:15 +0530

    Add files via upload

---
+++ ch_1_Vars_and_Datatypes/Literals.java
@@ -0,0 +1,22 @@
+package ch_1_Vars_and_Datatypes;
+
+
+public class L_04_literals {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        int item = 20;
+        short item1 = 55;
+        long item3 = 3666655446L;
+        char ch = 'A';
+        float f1 = 7.9f;
+        double d1 = 3.99d; // any num is bydefault being double
+        boolean y = true;
+        System.out.println(age);
+
+        String st = "Yashvi";
+        System.out.println(st);
+
+    }
+}
+
