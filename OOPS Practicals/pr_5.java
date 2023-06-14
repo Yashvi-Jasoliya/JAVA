47fd6b2f30611462ac4907705746ce0c7c09c992
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-14 21:56:26 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_5.java
@@ -0,0 +1,14 @@
+package clg_pr_1_to_10;
+
+public class pr_5 {
+    public static void main(String[] args) {
+
+        int n = 4;
+        for (int i = n; i!=0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("A ");
+            }
+            System.out.println("");
+        }
+    }
+}
