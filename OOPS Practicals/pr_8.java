380fd0b6156a0f04df7d3c57d2b033727feeb67b
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-15 18:51:40 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_8.java
@@ -0,0 +1,14 @@
+package clg_pr_1_to_10;
+
+public class pr_8 {
+    public static void main(String[] args) {
+
+        System.out.println("number " + "Square " + "Cube ");
+
+        for (int i= 1; i<=10;i++){
+            System.out.printf("%d, %d, %d\n", i, i*i , i*i*i);
+
+        }
+
+    }
+}
