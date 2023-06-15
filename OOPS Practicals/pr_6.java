e0753559b875853e193174a636eee820df4b3e7e
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-15 18:50:49 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_6.java
@@ -0,0 +1,18 @@
+package clg_pr_1_to_10;
+
+import java.util.Scanner;
+
+public class pr_6 {
+    public static void main(String[] args) {
+        System.out.println("Enter the numbers");
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+
+        Scanner sc2 = new Scanner(System.in);
+        double num2 = sc.nextDouble();
+        double prod = num1*num2;
+        System.out.printf("The product of %f * %f is :-  %f", num1,num2,prod);
+
+
+    }
+}
