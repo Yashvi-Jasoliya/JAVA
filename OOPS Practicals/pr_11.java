61837d4648c13935165d82f9ef3eb08ad2741274
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-15 18:53:02 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_11.java
@@ -0,0 +1,14 @@
+package clg_pr_11_to_14;
+
+public class pr_11 {
+    public static void main(String[] args) {
+        float[] arr = {50.3f, 55.36f, 98.36f, 41.0f, 87.36f};
+
+        System.out.println("Printing elements of Arrays :- ");
+        for (float element: arr){
+            System.out.println(element);
+        }
+        System.out.println("Length of array is :- " + arr.length);
+
+    }
+}
