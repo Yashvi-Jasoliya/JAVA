7e2bcbcdaac084f3337e4f608807879bc61862f6
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 18:43:42 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_25.java
@@ -0,0 +1,43 @@
+package clg_pr_24_to_31;
+
+//25
+    public class Complex {
+        public double real;
+        public double imag;
+
+        public Complex(double real, double imaginary) {
+            this.real = real;
+            this.imag = imaginary;
+        }
+
+        public Complex add(Complex second) {
+            double sum_Real = this.real + second.real;
+            double sum_Imag = this.imag + second.imag;
+            return new Complex(sum_Real, sum_Imag);
+        }
+
+        public boolean Compare(Complex second) {
+            boolean res = this.real == second.real && this.imag == second.imag;
+            return res;
+        }
+
+        public static void main(String[] args) {
+
+            Complex c_num1 = new Complex(10, 8.0);
+            Complex c_num2 = new Complex(10, 8.0);
+
+            Complex sum = c_num1.add(c_num2);
+            System.out.println("Sum of complex number 1 and 2:- " + sum.real + " + " + sum.imag + "i");
+
+            boolean Comp_res = c_num1.Compare(c_num2);
+
+            if(Comp_res == true){
+                System.out.println("Complex numbers are equal..");
+            }
+            else{
+                System.out.println("Complex numbers are different..");
+            }
+
+        }
+    }
+
