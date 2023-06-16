3c6dd950d846602d97eefeca5c4da747384ecd08
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 18:56:36 +0530

    Update pr_01.java

--- OOPS Practicals/pr_01.java
+++ OOPS Practicals/pr_01.java
@@ -8,11 +8,11 @@ package clg_pr_1_to_10;
 
 public class pr_01 {
     public static void main(String[] args) {
-
+      
         System.out.println("First Name : abc");
         System.out.println("Second Name : pqr");
         System.out.println("Last Name : str");
+      
         }
-
-    }
+  }
 
