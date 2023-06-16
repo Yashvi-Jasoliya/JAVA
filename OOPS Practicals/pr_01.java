f4be4f3e9aad047c7dba03460319c444915c6c34
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 18:57:13 +0530

    Update pr_01.java

--- OOPS Practicals/pr_01.java
+++ OOPS Practicals/pr_01.java
@@ -1,10 +1,10 @@
-package clg_pr_1_to_10;
-
 /*  Pr:-1 --> Pass student name using command line argument and print output in following format:     
               First Name : abc
               Second Name : pqr
               Last Name : str       */
 
+
+package clg_pr_1_to_10;
 
 public class pr_01 {
     public static void main(String[] args) {
