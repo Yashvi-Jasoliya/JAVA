a038d4884d73cf1d99296314798b84dba18f7683
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 19:06:42 +0530

    Update pr_08.java

--- OOPS Practicals/pr_08.java
+++ OOPS Practicals/pr_08.java
@@ -1,3 +1,7 @@
+
+/* Pr:-8 --> Write a program that outputs a table of numbers. Each line in the table contains three entries: the number,
+             its square, and its cube. Begin with 1 and end with 10.  */
+
 package clg_pr_1_to_10;
 
 public class pr_08 {
