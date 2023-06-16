c8ca1900500763cb401ad02cd0a17477a967a364
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 18:39:33 +0530

    Add files via upload

---
+++ OOPS Practicals/pr_13.java
@@ -0,0 +1,25 @@
+package clg_pr_11_to_14;
+
+import java.util.Scanner;
+
+public class pr_13 {
+    public static void main(String[] args) {
+
+            int[] arr = {100, 58, 21, 89};
+             System.out.println("Enter the element which you search :- ");
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+
+            for (int element: arr){
+                if (num == element){
+                    System.out.println(num + " is found in Array");
+                    break;
+                }
+                else {
+                    System.out.println(num + " is not found in Array");
+                    break;
+                }
+
+            }
+    }
+}
