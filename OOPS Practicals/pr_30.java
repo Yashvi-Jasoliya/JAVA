255ea9b1a6611f4472ec471a1603e3c03a814c1d
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 19:27:22 +0530

    Update pr_30.java

--- OOPS Practicals/pr_30.java
+++ OOPS Practicals/pr_30.java
@@ -1,6 +1,9 @@
-package clg_pr_24_to_31;
 
-//30
+/* Pr:-30 --> Write a program that illustrates method overriding. Class Bond is extended by ConvertibleBond. Each of there classes defines a display() method that 
+              outputs the string “Bond” of “ConvertibleBond”, respectively. Declare an array to hold six Bond objects. Initialize the elements of the array with a mix 
+              of Bond and ConvertibleBond objects. Execute a program loop to invoke the display method of each object. */
+
+package clg_pr_24_to_31;
 
  class Bond {
         public void display() {
