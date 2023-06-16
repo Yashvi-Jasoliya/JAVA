d29f57cc9308a87417940736255be80754f1ab0b
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 19:13:50 +0530

    Update pr_16.java

--- OOPS Practicals/pr_16.java
+++ OOPS Practicals/pr_16.java
@@ -1,3 +1,8 @@
+
+/* Pr:-16 --> Write a program that will accept a string “java123is234fun” will replace all occurrences of digit with single space character in 
+              a new string. (use of replaceAll() function and regular expression for digit).Split the new formed string from space character and form the array of 
+              strings (use of split function). Display strings from the array in new line each.  */
+
 package clg_pr_15_to_23;
 
 public class pr_16 {
