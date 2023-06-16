adeb1815605363c2901f381a98a3c8d59346691e
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-16 19:05:44 +0530

    Update pr_07.java

--- OOPS Practicals/pr_07.java
+++ OOPS Practicals/pr_07.java
@@ -1,3 +1,8 @@
+
+ /*  Pr:-7 --> Write an application that converts between meters and feet. its first command-line argument is a number. Its second command-line
+               argument if either “feet” or “meters”. If this argument equal “feet”, display a string reporting the equivalent number of meters. 
+               If this argument equal “meters”, display a string reporting the equivalent number of feet. Otherwise, report that the unit system is not recognized. (note: one meter is equal to 3.28 feet.)  */
+
 package clg_pr_1_to_10;
 
 import java.util.Scanner;
