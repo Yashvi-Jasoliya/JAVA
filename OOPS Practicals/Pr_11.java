626d6de408d0c8c17d41856aa5230b4f38758848
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-19 22:48:07 +0530

    Update and rename pr_11.java to Pr_11.java

--- OOPS Practicals/Pr_11.java
+++ OOPS Practicals/Pr_11.java
@@ -3,7 +3,7 @@
 
 package clg_pr_11_to_14;
 
-public class pr_11 {
+public class Pr_11 {
     public static void main(String[] args) {
         float[] arr = {50.3f, 55.36f, 98.36f, 41.0f, 87.36f};
 
