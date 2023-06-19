fad732873b2023d1095d534ad6be988455477ea3
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-19 22:37:12 +0530

    Update and rename pr_06.java to Pr_06.java

--- OOPS Practicals/Pr_06.java
+++ OOPS Practicals/Pr_06.java
@@ -5,7 +5,7 @@ package clg_pr_1_to_10;
 
 import java.util.Scanner;
 
-public class pr_06 {
+public class Pr_06 {
     public static void main(String[] args) {
         
         System.out.println("Enter the numbers");
