586d8a0e41fc95ca8c1afb7bc1c624a437730f52
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-20 21:41:45 +0530

    Update and rename pr_14.java to Pr_14.java

--- OOPS Practicals/Pr_14.java
+++ OOPS Practicals/Pr_14.java
@@ -5,7 +5,7 @@ package clg_pr_11_to_14;
 
 import java.util.Scanner;
 
-public class pr_14 {
+public class Pr_14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the raw :- ");
