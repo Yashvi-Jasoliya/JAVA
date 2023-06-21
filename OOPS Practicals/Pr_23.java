2426ea50d723f3791840d5456e0ff7e96298ddd3
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-21 21:27:29 +0530

    Update and rename pr_23.java to Pr_23.java

--- OOPS Practicals/Pr_23.java
+++ OOPS Practicals/Pr_23.java
@@ -5,7 +5,7 @@ package clg_pr_15_to_23;
 
 import java.util.Scanner;
 
-public class pr_23 {
+public class Pr_23 {
     public static void main(String[] args) {
         System.out.println("Enter the string 1 ");
         Scanner sc1 = new Scanner(System.in);
