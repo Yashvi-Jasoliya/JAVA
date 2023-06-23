22a0d984cc9fc69ff49a871d3bcdddeb4bb44c24
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-23 10:05:56 +0530

    Add files via upload

---
+++ ch_2_Operator_and_Expression/Operators.java
@@ -0,0 +1,33 @@
+package ch_2_Operator_and_Expression;
+
+public class L_08_operators {
+    public static void main(String[] args) {
+//      1.  Arithmatic operator
+        int a = 10;
+//      int b = 5 * 10;
+//      int b = 5 % a;
+//      4.8%1.1 --> Returns Decimal Reminder..
+
+//      2.  Assignment operator
+        int b = 8;
+        b += 5;
+        System.out.println(b);
+
+//      3. Comparison operators
+//      System.out.println(10==20);
+//      System.out.println(6>3);
+
+//      4.  Logical operator
+//      System.out.println(23>98 && 23<98);
+        System.out.println(23>98 || 23<98);
+
+//       5. Bitwise operator
+        System.out.println(2&3);
+//
+//        10
+//        11
+//     ----------
+//        10
+
+    }
+}
