eb29570cab05cba4920666b737b2fe0792c56b63
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-23 10:08:40 +0530

    Add files via upload

---
+++ ch_2_Operator_and_Expression/Op_Precedence_and_Assoc.java
@@ -0,0 +1,37 @@
+package ch_2_Operator_and_Expression;
+
+public class L_09_op_Precedence_and_Assoc {
+    public static void main(String[] args) {
+    /* int a = 6*8-50/2 ;
+//    = 48-50/2
+//    = 48-25
+//    = 23 --> According precedence High/Low..
+
+        int b = 50/2-6*8;
+//        = 25-6*8
+//        = 25-48
+//        = -23   --> According Associativity Left to Right
+
+        System.out.println(a);
+        System.out.println(b);
+*/
+
+//   -->   Quick quize
+//        int a = 5, b = 2;
+//        int c = a * b/2;
+//        System.out.println(c);
+
+//        int a = 2, b = 3, c = 6;
+//        int x = (b*b - 4*a*c) / (2*a);
+//        System.out.println(x);
+
+//         int u = 5, v = 2;
+//         int c = v*v - u*u;
+//        System.out.println(c);
+
+        int a = 2, b = 5, c = 4;
+        int x = a * b - c;
+        System.out.println(x);
+
+    }
+}
