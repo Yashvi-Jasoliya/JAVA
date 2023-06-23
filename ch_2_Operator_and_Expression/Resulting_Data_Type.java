5110326814811a830918188a8dee7f4bb421c64f
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-06-23 10:07:50 +0530

    Add files via upload

---
+++ ch_2_Operator_and_Expression/Resulting_Data_Type.java
@@ -0,0 +1,41 @@
+package ch_2_Operator_and_Expression;
+
+public class L_10_Resulting_Data_Type {
+    public static void main(String[] args) {
+
+    /*   byte a = 4;
+        int b = 5;
+        short c = 2;
+        int x = b + c;
+        float y = 3.25555555f + a;
+        System.out.println(x);
+        System.out.println(y);  */
+
+//        Increament and Decrement
+       /* int i = 50;
+        System.out.println(i++);  // first assign/print then after increament
+        System.out.println(i);
+        System.out.println(++i);  // first increament then after print
+        System.out.println(i);  */
+
+//      Quick quize
+    /*  int i = 40;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(i--);
+        System.out.println(i);
+        System.out.println(--i);
+        System.out.println(i);  */
+
+//      Quick quize
+        int y = 7;
+        int x = ++y * 8;  // ++ has high precdence than *
+        System.out.println(x);
+
+    char ch = 'B';
+        System.out.println(++ch);
+
+    }
+}
