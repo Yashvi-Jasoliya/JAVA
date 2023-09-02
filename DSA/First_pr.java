3ab0144056689793bc602d677dd5972f239b9e1b
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-09-02 22:19:35 +0530

    Add files via upload

---
+++ DSA/First_pr.java
@@ -0,0 +1,8 @@
+package DSA_IN_JAVA;
+
+public class First_pr {
+    public static void main(String[] args) {
+        System.out.print("Hello! DSA\n");
+        System.out.println("Hii!");
+    }
+}
---
+++ DSA/Patterns.java
@@ -0,0 +1,9 @@
+package DSA_IN_JAVA;
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
