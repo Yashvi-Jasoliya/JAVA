4abcded3ab0d2d246a28875ddac52f33707ed122
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-09-03 20:35:49 +0530

    Add files via upload

---
+++ DSA/User_Input.java
@@ -0,0 +1,23 @@
+ package DSA_IN_JAVA;
+
+import java.util.Scanner;
+
+public class User_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        Printing only first word
+//        String input = sc.next();
+
+//        Printing whole sentence
+//        String input = sc.nextLine();
+//        System.out.println(input);
+
+//      Printing integer
+        int num = sc.nextInt();
+        System.out.println(num);
+
+//        Gives true or false
+//        boolean b1 = sc.hasNextFloat();
+//        System.out.println(b1);
+    }
+}
