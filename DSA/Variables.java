a41d6ff128e442760f461fa9b2f484894ad40dcc
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-09-04 22:23:11 +0530

    Add files via upload

---
+++ DSA/Variables.java
@@ -0,0 +1,30 @@
+package DSA_IN_JAVA;
+
+public class Variables {
+    public static void main(String[] args) {
+//        int a = 50;
+//        int b = 60;
+//        System.out.println(a);
+//        System.out.println(b);
+//        String name = "yashvi";
+//        System.out.println(name);
+//
+//        a = 100;
+//        System.out.println(a);
+//
+//        a = b;
+//        System.out.println(a);
+
+        byte b =  127;  // Range -128 to 127 --> 1 byte
+        System.out.println(b);
+        char ch = 'A';   // 2 bytes
+        System.out.println(ch);
+        boolean b1 = true; // 1 byte
+        float f = 5.3f;    // 4 bytes
+        int i = 12;   // 4 bytes
+//        long  --> 8 bytes
+//        double --> 8 bytes
+//        short  --> 2 bytes
+
+    }
+}
