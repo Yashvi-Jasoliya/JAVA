6c7f7a1f9585aaedc91257f212e8f9c1d209e35d
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-09-11 20:45:49 +0530

    Add files via upload

---
+++ DSA/Questions_2.java
@@ -0,0 +1,51 @@
+        package DSA_IN_JAVA;
+
+        public class Questions_2 {
+            public static void main(String[] args) {
+
+        //        pr 1
+
+        //        int x = 2, y = 5;
+        //        int exp1 = (x * y / x);
+        //        int exp2 = (x * (y / x));
+        //        System.out.print(exp1 + ",");
+        //        System.out.print(exp2);
+
+        //        pr 2
+
+        //        int x = 200, y = 50, z = 100;
+        //        if(x > y && y > z){
+        //            System.out.println("Hello");
+        //        }
+        //        if(z > y && z < x){
+        //            System.out.println("Java");
+        //        }
+        //        if((y+200) < x && (y+150) < z){
+        //            System.out.println("Hello Java");
+        //    }
+
+        //        pr 3
+
+        //                int x, y, z;
+        //                x = y = z = 2;
+        //                x += y;
+        //                y -= z;
+        //                z /= (x + y);
+        //                System.out.println(x + " " + y + " " + z);
+
+        //        pr 4
+
+//                int x = 9, y = 12;
+//                int a = 2, b = 4, c = 6;
+//                int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+//                System.out.println(exp);
+
+//                pr 5
+
+                int x = 10, y = 5;
+                int exp1 = (y * (x / y + x / y));
+                int exp2 = (y * x / y + y * x / y);
+                System.out.println(exp1);
+                System.out.println(exp2);
+            }
+        }
